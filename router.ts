4db0d754b2a9969b70a7bfe044d2a9715d689d17
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2024-04-18 13:09:53 +0100

    Ups

--- router.ts
+++ router.ts
@@ -9,33 +9,194 @@ type Method =
 	| 'TRACE'
 	| 'PATCH';
 
-type MaybePromise<Type> = Type | Promise<Type>;
-type Pattern = string;
-type Groups = URLPatternComponentResult['groups'];
-type NextHandler = () => Promise<Response>;
+type MaybePromise<Type> = Promise<Type> | Type;
 
-export type Handler = (
+type Groups = URLPatternComponentResult['groups'];
+type Next<Data> = (data?: Data) => Promise<Response>;
+
+type Middleware<Data> = (
 	request: Request,
-	group: Groups,
-	next: NextHandler,
+	groups: Groups,
+	data: Data,
+	next: Next<Data>,
+) => MaybePromise<void>;
+
+type Handler<Data> = (
+	request: Request,
+	groups: Groups,
+	data: Data,
 ) => MaybePromise<Response>;
 
-class Route {
-	fast_children = new Map<Pattern, Route>();
-	slow_children = new Map<Pattern, Route>();
-	handler: Handler | null = null;
+type NotFoundHandler<Data> = (
+	request: Request,
+	data: Data,
+) => MaybePromise<Response>;
+
+type Pattern = string;
+
+function parts(pattern: Pattern) {
+	// NOTE root pathnames always trail
+	return pattern === '/' ? [''] : pattern.split('/');
 }
 
-function is_middleware(handler: Handler) {
-	return handler.length > 2;
+function is_wildcard(part: string) {
+	return part === '*';
 }
 
 function is_fast(part: string) {
-	return !part.includes('[');
+	return part[0] !== ':';
 }
 
-export class Router {
-	#routes: Record<Method, Route> = {
+function create_route<Data>(current_route: Route<Data>, part: string) {
+	if (is_wildcard(part)) {
+		return current_route.wildcard_child ??= new Route();
+	}
+
+	if (is_fast(part)) {
+		if (!current_route.fast_children.has(part)) {
+			current_route.fast_children.set(part, new Route());
+		}
+
+		return current_route.fast_children.get(part)!;
+	}
+
+	// is slow
+	const group = part.slice(1);
+
+	if (!current_route.slow_children.has(group)) {
+		current_route.slow_children.set(group, new Route());
+	}
+
+	return current_route.slow_children.get(group)!;
+}
+
+function create_path<Data>(current_route: Route<Data>, pattern: string) {
+	for (const part of parts(pattern)) {
+		current_route = create_route(current_route, part);
+	}
+
+	return current_route;
+}
+
+function is_middleware<Data>(handler: Middleware<Data> | Handler<Data>) {
+	return handler.length > 3;
+}
+
+function compose<Data>(first: Middleware<Data>, second: Middleware<Data>) {
+	return async (
+		request: Request,
+		groups: Groups,
+		data: Data,
+		next: Next<Data>,
+	) => {
+		let response: Response;
+
+		await first(
+			request,
+			groups,
+			data,
+			async (next_data = data) => {
+				await second(
+					request,
+					groups,
+					next_data,
+					async () => response = await next(),
+				);
+
+				return response;
+			},
+		);
+
+		return response!;
+	};
+}
+
+function is_destination(parts: string[]) {
+	return parts.length === 0;
+}
+
+class Route<Data> {
+	middleware: Middleware<Data>[] = [];
+	handler?: Handler<Data>;
+	not_found_handler?: NotFoundHandler<Data>;
+	fast_children = new Map<Pattern, this>();
+	slow_children = new Map<Pattern, this>();
+	wildcard_child?: Route<Data>;
+}
+
+interface Entry {
+	type: string;
+	// deno-lint-ignore ban-types
+	callback: Function;
+	groups: Groups;
+}
+
+interface MiddlewareEntry<Data> extends Entry {
+	type: 'middleware';
+	callback: Middleware<Data>;
+}
+
+interface HandlerEntry<Data> extends Entry {
+	type: 'handler';
+	callback: Handler<Data>;
+}
+
+interface NotFoundEntry<Data> extends Entry {
+	type: 'not_found';
+	callback: NotFoundHandler<Data>;
+}
+
+type EntryKind<Data> =
+	| MiddlewareEntry<Data>
+	| HandlerEntry<Data>
+	| NotFoundEntry<Data>;
+
+function* traverse_tree<Data>(
+	[part, ...parts]: string[],
+	current_route: Route<Data>,
+): Generator<EntryKind<Data>, void, void> {
+	// TODO
+	if (part === undefined) {
+		for (const middleware of current_route.middleware) {
+			yield { type: 'middleware', callback: middleware, groups: {} };
+		}
+
+		if (current_route.handler) {
+			yield { type: 'handler', callback: current_route.handler, groups: {} };
+		}
+
+		if (current_route.not_found_handler) {
+			yield {
+				type: 'not_found',
+				callback: current_route.not_found_handler,
+				groups: {},
+			};
+		}
+
+		return;
+	}
+
+	if (current_route.wildcard_child) {
+		yield* traverse_tree([], current_route.wildcard_child);
+	}
+
+	for (const [group, child] of current_route.slow_children) {
+		// const groups = { [group]: part };
+
+		// console.log(groups);
+
+		yield* traverse_tree(parts, child);
+	}
+
+	if (current_route.fast_children.has(part)) {
+		yield* traverse_tree(parts, current_route.fast_children.get(part)!);
+	}
+}
+
+export class Router<
+	Data extends Record<string | number | symbol, unknown>,
+> {
+	#routes: Record<Method, Route<Data>> = {
 		GET: new Route(),
 		HEAD: new Route(),
 		POST: new Route(),
@@ -47,41 +208,31 @@ export class Router {
 		PATCH: new Route(),
 	};
 
+	#default_data: Data;
+
+	constructor(data: Data) {
+		this.#default_data = data;
+	}
+
 	#add_route(
-		root_route: Route,
+		root_route: Route<Data>,
 		pattern: Pattern,
-		handler: Handler,
+		handler: Middleware<Data> | Handler<Data>,
 	) {
-		// NOTE root pathnames always trail
-		const parts = pattern === '/' ? [''] : pattern.split('/');
-		let current_route = root_route;
+		const current_route = create_path(root_route, pattern);
 
-		for (const part of parts) {
-			if (is_fast(part)) {
-				if (!current_route.fast_children.has(part)) {
-					current_route.fast_children.set(part, new Route());
-				}
-
-				current_route = current_route.fast_children.get(part)!;
-
-				continue;
-			}
-
-			const group = part.slice(1, -1);
-
-			if (!current_route.slow_children.has(part)) {
-				current_route.slow_children.set(group, new Route());
-			}
-
-			current_route = current_route.slow_children.get(group)!;
+		if (is_middleware(handler)) {
+			current_route.middleware.push(handler as Middleware<Data>);
+		} else {
+			current_route.handler = handler as Handler<Data>;
 		}
-
-		current_route.handler = handler;
 
 		return this;
 	}
 
-	all(pattern: Pattern, handler: Handler) {
+	all(pattern: Pattern, handler: Middleware<Data>): this;
+	all(pattern: Pattern, handler: Handler<Data>): this;
+	all(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		for (const method of Object.values(this.#routes)) {
 			this.#add_route(method, pattern, handler);
 		}
@@ -89,84 +240,120 @@ export class Router {
 		return this;
 	}
 
-	get(pattern: Pattern, handler: Handler) {
+	get(pattern: Pattern, handler: Middleware<Data>): this;
+	get(pattern: Pattern, handler: Handler<Data>): this;
+	get(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.GET, pattern, handler);
 	}
 
-	head(pattern: Pattern, handler: Handler) {
+	head(pattern: Pattern, handler: Middleware<Data>): this;
+	head(pattern: Pattern, handler: Handler<Data>): this;
+	head(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.HEAD, pattern, handler);
 	}
 
-	post(pattern: Pattern, handler: Handler) {
+	post(pattern: Pattern, handler: Middleware<Data>): this;
+	post(pattern: Pattern, handler: Handler<Data>): this;
+	post(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.POST, pattern, handler);
 	}
 
-	put(pattern: Pattern, handler: Handler) {
+	put(pattern: Pattern, handler: Middleware<Data>): this;
+	put(pattern: Pattern, handler: Handler<Data>): this;
+	put(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.PUT, pattern, handler);
 	}
 
-	delete(pattern: Pattern, handler: Handler) {
+	delete(pattern: Pattern, handler: Middleware<Data>): this;
+	delete(pattern: Pattern, handler: Handler<Data>): this;
+	delete(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.DELETE, pattern, handler);
 	}
 
-	connect(pattern: Pattern, handler: Handler) {
+	connect(pattern: Pattern, handler: Middleware<Data>): this;
+	connect(pattern: Pattern, handler: Handler<Data>): this;
+	connect(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.CONNECT, pattern, handler);
 	}
 
-	options(pattern: Pattern, handler: Handler) {
+	options(pattern: Pattern, handler: Middleware<Data>): this;
+	options(pattern: Pattern, handler: Handler<Data>): this;
+	options(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.OPTIONS, pattern, handler);
 	}
 
-	trace(pattern: Pattern, handler: Handler) {
+	trace(pattern: Pattern, handler: Middleware<Data>): this;
+	trace(pattern: Pattern, handler: Handler<Data>): this;
+	trace(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.TRACE, pattern, handler);
 	}
 
-	patch(pattern: Pattern, handler: Handler) {
+	patch(pattern: Pattern, handler: Middleware<Data>): this;
+	patch(pattern: Pattern, handler: Handler<Data>): this;
+	patch(pattern: Pattern, handler: Middleware<Data> | Handler<Data>) {
 		return this.#add_route(this.#routes.PATCH, pattern, handler);
 	}
 
-	async #handle(
-		request: Request,
-		current_route: Route | undefined,
-		[part, ...parts]: string[],
-	): Promise<Response> {
-		const fast_route = current_route?.fast_children.get(part);
-		const is_fast = fast_route !== undefined;
-		let key;
+	not_found(pattern: Pattern, handler: NotFoundHandler<Data>) {
+		// TODO other methods
+		const current_route = create_path(this.#routes.GET, pattern);
 
-		if (is_fast) {
-			current_route = fast_route;
-		} else {
-			[key, current_route] = current_route?.slow_children.entries().next()
-				.value ?? [];
-		}
+		current_route.not_found_handler = handler;
 
-		if (!current_route) return new Response(null, { status: 404 });
-
-		if (!current_route.handler) {
-			return await this.#handle(request, current_route, parts);
-		}
-
-		if (is_middleware(current_route.handler)) {
-			return await current_route.handler(request, async () => {
-				return await this.#handle(request, current_route, parts);
-			});
-		}
-
-		if (parts.length === 0) {
-			const groups = is_fast ? undefined : { [key]: part };
-
-			return await current_route.handler(request, groups);
-		}
-
-		return await this.#handle(request, current_route, parts);
+		return this;
 	}
 
-	handle: Deno.ServeHandler = async (request) => {
-		const current_route = this.#routes[request.method as Method];
-		const { pathname } = new URL(request.url);
-		const parts = pathname === '/' ? [''] : pathname.split('/');
+	async handle(request: Request) {
+		const root_route = this.#routes[request.method as Method];
+		// TODO request vs node scoped, currently request
+		const groups: Groups = {};
+		const data = Object.assign({}, this.#default_data);
 
-		return await this.#handle(request, current_route, parts);
-	};
+		const { pathname } = new URL(request.url);
+
+		let middleware: Middleware<Data> = (request, groups, data, next) =>
+			next(data);
+		let handler: Handler<Data> | undefined;
+		let not_found_handler: NotFoundHandler<Data> = () =>
+			new Response(null, { status: 404 });
+
+		for (const entry of traverse_tree(parts(pathname), root_route)) {
+			switch (entry.type) {
+				case 'middleware': {
+					middleware = compose(middleware, entry.callback);
+
+					break;
+				}
+
+				case 'handler': {
+					handler = entry.callback;
+
+					break;
+				}
+
+				case 'not_found': {
+					not_found_handler = entry.callback;
+					break;
+				}
+
+				default:
+					entry satisfies never;
+			}
+		}
+
+		return await middleware(
+			request,
+			{},
+			this.#default_data,
+			async (next_data) => {
+				const new_data = next_data ? Object.assign(data, next_data) : data;
+
+				if (handler) {
+					return await handler(request, {}, new_data);
+				}
+
+				return await not_found_handler(request, new_data);
+			},
+		);
+	}
 }
