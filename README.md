d50a0b505947d197e86ac1603bf389e2cb2b9c47
Ayo Reis <84969551+ayoreis@users.noreply.github.com>
2024-04-18 13:07:55 +0100

    Middleware

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # [The Photography Database](https://tpdb.ayoreis.com)
 
 Work in progress.
+
+## Todo
+
+- [ ] Destination
+- [ ] Other parts of URL
+- [ ] Groups
+- [ ] Can middleware return
+- [ ] `/*` match self
--- mod.ts
+++ mod.ts
@@ -1,7 +1,30 @@
-import { FileSystemRouter } from './file-system-router.ts';
+import { Router } from './router.ts';
 
-const router = new FileSystemRouter('routes');
+const router = new Router({});
 
-await router.recurse_directory();
+router.get('/*', async (_request, _group, _data, next) => {
+	console.log('/ middleware started, next');
+	await next();
+	console.log('/ middleware finished');
+});
 
-Deno.serve(router.handle);
+router.get('/a/*', async (_request, _group, _data, next) => {
+	console.log('/a/* middleware started, next\n');
+	await next();
+	console.log('/a/* middleware finished');
+});
+
+router.get('/:x/*', async (_request, _group, _data, next) => {
+	console.log('/:x/* middleware called, next');
+	await next();
+	console.log('/:x/* middleware finished');
+});
+
+router.not_found('*', () => {
+	return new Response('Page not found, 404', { status: 404 });
+});
+
+router.get('/a/b', () => new Response(`Hello world!`));
+router.get('/a/:name', (_request, { name }) => new Response(`Hello ${name}!`));
+
+Deno.serve(async (request: Request) => await router.handle(request));
